163935e49d74e145ff39ad26dba65bb0ebd39672
SWERgroup <plugins@swergroup.com>
2013-05-25 19:21:46 +0200

    cleaning, bugfixing, 330 alpha

--- inc/wp-cli.class.php
+++ inc/wp-cli.class.php
@@ -4,23 +4,23 @@
  *
  * @package wp-cli
  */
-class Test_Greeklish extends WP_CLI_Command {
+class UploadPlus_Tests extends WP_CLI_Command {
 
 	/**
 	 * Greeklish test ( "Αισθάνομαι τυχερός" in "esthanome ticheros" )
 	 * 
 	 * @synopsis <string>
 	 */
-	function testgreeklish( $args, $assoc_args ) {
+	function greeklish( $args, $assoc_args ) {
 		list( $string ) = $args;
 
 		$convert = SWER_uploadplus_core::sanitize_greeklish( $string );
 
 		// Print a success message
-		WP_CLI::success( "input: " . $string . " | output: " . $convert );
+		WP_CLI::success( 'input: ' . $string . ' | output: ' . $convert );
 	}
 }
 
-WP_CLI::add_command( 'uploadplus', 'Test_Greeklish' );
+WP_CLI::add_command( 'upp', 'UploadPlus_Tests' );
 
 ?>
